290f94d04e3fd0ef3253889f026d8bcc0fd20ced
Brandon Jackson <usbrandon@gmail.com>
2025-03-19 00:58:47 -0500

    Added a sample runner

---
+++ hop-environment/run-hop-sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/opt/hop/hop-run.sh \ 
+	--environment="Development" \
+       	--project="samples" \
+       	--file=/opt/hop/config/projects/samples/transforms/fake-data-generate-person-record.hpl
